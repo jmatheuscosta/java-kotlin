cff1ce276aace267bbf78816df76d193f8336ff1
Matheus Costa <jmatheuscosta17@gmail.com>
2024-11-04 08:44:33 -0300

    encapsulamento java

---
+++ POOEncapsulamento/app/src/main/java/br/com/matheus/encapsulamento/book.java
@@ -0,0 +1,9 @@
+package br.com.matheus.encapsulamento;
+
+public class book {
+    private String title;
+    private String author;
+    private String editor;
+    private int pages;
+    private String location;
+}
